a67ac428ed8cf0922da16d8a9758402b7165527c
Nicolas Schmid <nicolas@breiten.ch>
2020-05-21 16:14:42 +0200

    feat: scroll timeline

--- project/static/js/src/index.js
+++ project/static/js/src/index.js
@@ -65,23 +65,78 @@ var someFunc2 = function() {
         oncreate: (ctrl) => {
             let offset = myMap.map.getSize().x*0.15;
             anime({
-                targets: ctrl.dom,
+                targets: ctrl.dom.querySelector('.slide1'),
                 opacity: [0, 1],
                 duration: 2000,
-                easing: "easeInOutQuad"
+                easing: "easeInOutQuad",
+                begin: () => {
+                    ctrl.dom.querySelector('.slide1').style.display = 'block';
+                }
             });
+            timeline = anime.timeline({
+                autoplay: false,
+                duration: 4000,
+            });
+            timeline.add({
+                targets: document.querySelector('.slide1'),
+                opacity: [1, 0],
+                translateY: [0, -300],
+                duration: 1000,
+                easing: 'easeOutQuad',
+                update: (anim) => {
+                    if (anim.progress < 25){
+                        removeall();
+                        document.querySelector('.rs-menu-stein').classList.add('active')
+                    }
+                }
+            }, 0)
+            timeline.add({
+                targets: document.querySelector('.slide2'),
+                opacity: [0,1],
+                translateY: [300, 0],
+                duration: 1000,
+                easing: 'easeInQuad',
+                begin: (anim) => {
+                    anim.animatables[0].target.style.display = 'block';
+                },
+                update: (anim) => {
+                    if (anim.progress > 25 && anim.progress < 75){
+                        removeall();
+                        document.querySelector('.rs-menu-fundort').classList.add('active')
+                    }
+                }
+            }, 0)
         },
         view() {
             return (
-                <div class="rs-content">
+            <div>
+                <div class="rs-content slide1">
                     <img src="/static/img/steine/Beispiel_1a_Steinaufnahme.png" />
                     <h1>Schwarzes Glitzern</h1>
                 </div>
+                <div class="rs-content slide2">
+                    <img src="/static/img/steine/Beispiel_1b_Naturaufnahme.jpg" />
+                </div>
+                <div class="rs-content slide3">
+                    Der Stein ist von einem Strand in Dänemark, aus Gilleleje, ganz im Norden von Seeland, wo ich sehr gerne bin. Ich liebe es, am Strand Steine zu sammeln. Ich suche nach schönen Steinen oder solchen, die eine Form haben, die mich an etwas erinnert: Ein Tier, ein Herz, ein Haus, ganz egal. Damit gestalte ich Bilder, fülle eine Vase oder lege sie einfach aus zum Ansehen.
+                </div>
+                <div class="rs-content slide4">
+                Granatamphibolit
+                Gilleleje
+                Die Amphibole im Granatamphibolit lassen ihn sehr schwarz aussehen und im Licht glitzern. Die kleinen roten Einschlüsse sind Granate. Er stammt ursprünglich nicht aus Dänemark, sondern kommt wahrscheinlich aus Südschweden, wo diese Gesteine unter sehr hohen Temperaturen und Drucken vor vielen Millionen Jahren im Innern der Erde entstanden sind. Während der letzten Eiszeit wurde er dann mit einem Gletscher ins heutige Dänemark transportiert. Durch Abrieb im Gletscher- und Meereswasser erhielt er seine runde Form. 
+                </div>
+            </div>
             )
         }
     }
 }
 
+var removeall = () => {
+    document.querySelector('.rs-menu-stein').classList.remove('active');
+    document.querySelector('.rs-menu-fundort').classList.remove('active');
+    document.querySelector('.rs-menu-geschichte').classList.remove('active');
+    document.querySelector('.rs-menu-geologie').classList.remove('active');
+}
 
 var someFunc3 = function () {
     return{
@@ -113,13 +168,72 @@ var someFunc3 = function () {
 
                     <div>
 
-                        <h4 class="active">Stein</h4> <h4>Fundort</h4> <h4>Geschichte</h4> <h4>Geologie</h4>
+                        <h4 class="active rs-menu-stein">Stein</h4> <h4 class="rs-menu-fundort">Fundort</h4> 
+                        <h4 class="rs-menu-geschichte">Geschichte</h4> <h4 class="rs-menu-geologie">Geologie</h4>
                     </div>
                 </div>
             )
         }
     }
 }
+
+var scrollY = 0;
+var maxHeight= 3000;
+
+var _event = {
+    y: 0,
+    deltaY: 0
+  };
+var percentage = 0
+
+function onWheel (e) {
+    // for embedded demo
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    e.stopPropagation();
+
+    var evt = _event;
+    evt.deltaY = e.wheelDeltaY || e.deltaY * -1;
+
+    scroll(e);
+
+    percentage = evt.y * -1 / maxHeight;
+    document.querySelector('.rs-scroll').innerHTML = 'scroll Y : ' + evt.deltaY + '  ' + evt.y;
+    timeline.seek(percentage * 4500);
+};
+
+function scroll (e) {
+  var evt = _event;
+  // limit scroll top
+  if ((evt.y + evt.deltaY) > 0 ) {
+    evt.y = 0;
+  // limit scroll bottom
+  } else if ((-(evt.y + evt.deltaY)) >= maxHeight) {
+    evt.y = -maxHeight;
+  } else {
+      evt.y += evt.deltaY;
+  }
+  scrollY = -evt.y
+}
+
+window.addEventListener('wheel', onWheel, { passive: false });
+
+var scrollview = function() {
+    return {   
+        view() {
+            return (
+                <div class="rs-scroll" style="position:absolute; top: 50%; left: 50%; color: black; z-index: 3000;">
+                </div>
+            )
+        }
+    }
+}
+
+m.mount(document.getElementById('rs-scroll'), scrollview);
+
+var timeline = null;
+
+
 
 // let myVar = new UserList()
 
