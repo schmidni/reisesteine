84d22debf58b3f2dbf44408ef25790d78c55b07c
Nicolas Schmid <nicolas@breiten.ch>
2020-07-03 10:30:46 +0200

    fix: disable eventlisteners, try map pos fixed

--- project/static/js/src/components/WorldMap.js
+++ project/static/js/src/components/WorldMap.js
@@ -67,12 +67,12 @@ export default class worldMap {
             mymap.zoomControl.setPosition('bottomleft');
         this.initMap(mymap);
 
-        window.addEventListener('touchend', debounce(() => {
-            const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-            document.getElementById('map').style.height = vh + 'px';
-            mymap.invalidateSize();
-            this.frame.updateFrame();
-        }, 20));
+        // window.addEventListener('touchend', debounce(() => {
+        //     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        //     document.getElementById('map').style.height = vh + 'px';
+        //     mymap.invalidateSize();
+        //     this.frame.updateFrame();
+        // }, 20));
         // window.addEventListener('resize', debounce(() => {
         //     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
         //     document.getElementById('map').style.height = vh + 'px';
